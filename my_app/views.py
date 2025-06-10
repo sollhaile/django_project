23ddbc3e55f309434ec4987c6c027d706372e629
sollhaile <sollhaile192@gmail.com>
2025-06-10 15:19:51 +0300

    my_project/urls.py modification

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,13 @@
-from django.http import HttpResponse
-def test_view(request):
-    return HttpResponse("TEST VIEW WORKS!")  # Simple response
+#from django.http import HttpResponse
+#def test_view(request):
+#    return HttpResponse("TEST VIEW WORKS!")  # Simple response
+
+from rest_framework import generics
+from models import Book
+from serializers import BookSerializer
+class BookListCreateAPIVIEW(generics.ListCreateAPIView):
+    """
+    API view to retrieve list of books or create a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
--- my_project/urls.py
+++ my_project/urls.py
@@ -1,12 +1,14 @@
-
-from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
-from my_app.views import test_view  # Replace `my_app` with your app name
-def test_view(request):
-    return HttpResponse("DIRECT TEST WORKS!")
+from django.contrib import admin
+
+def hardcoded_test(request):
+    return HttpResponse("HARDCODED TEST WORKS", content_type="text/plain")
+from my_app.views import test_view
 
 urlpatterns = [
+    path('hardcoded-test/', hardcoded_test),
     path('admin/', admin.site.urls),
-    path('api/test/', test_view, name='test_view'),  # Must be here
+    path('api/books/', views.BookListCreateAPIView.as_view(), name="book_list_create"),  # Your original view
+
 ]
