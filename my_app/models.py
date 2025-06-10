f5f2ab29e93070beea17337ea5f57c25753df03b
sollhaile <sollhaile192@gmail.com>
2025-06-10 11:48:21 +0300

    Intial Commit

---
+++ my_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from rest_framework import serializers
+from .models import Book
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
